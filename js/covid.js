ca64a24b9d034e364e8b424bcd4a86ea63f2c0ee
Ayuni Mahdhir <maizayuni@gmail.com>
2025-05-24 14:58:35 +0800

    Add COVID-19 cases/deaths chart to Covid dashboard

--- js/covid.js
+++ js/covid.js
@@ -1,7 +1,13 @@
 async function loadCovidData() {
   const url = "https://disease.sh/v3/covid-19/all";
-  const res = await fetch(url);
-  const data = await res.json();
+  const historicalUrl = "https://disease.sh/v3/covid-19/historical/all?lastdays=all";
+
+  const [currentRes, historicalRes] = await Promise.all([
+    fetch(url),
+    fetch(historicalUrl)
+  ]);
+  const data = await currentRes.json();
+  const historicalData = await historicalRes.json();
 
   const formatter = new Intl.NumberFormat('en-US');
 
@@ -19,6 +25,59 @@ async function loadCovidData() {
       <p class="text-3xl font-bold">${formatter.format(data.deaths)}</p>
     </div>
   `;
+
+  renderCovidChart(historicalData);
+}
+
+function renderCovidChart(historical) {
+  const dates = Object.keys(historical.cases);
+  const cases = Object.values(historical.cases);
+  const deaths = Object.values(historical.deaths);
+
+  const ctx = document.getElementById('covidChart').getContext('2d');
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          label: 'New Cases',
+          data: cases,
+          borderColor: 'rgba(59, 130, 246, 1)',
+          backgroundColor: 'rgba(59, 130, 246, 0.1)',
+          fill: true,
+          tension: 0.3
+        },
+        {
+          label: 'Deaths',
+          data: deaths,
+          borderColor: 'rgba(220, 38, 38, 1)',
+          backgroundColor: 'rgba(220, 38, 38, 0.1)',
+          fill: true,
+          tension: 0.3
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          labels: {
+            color: '#fff'
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: { color: '#ccc' }
+        },
+        y: {
+          ticks: { color: '#ccc' }
+        }
+      }
+    }
+  });
 }
 
 window.onload = loadCovidData;
